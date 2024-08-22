0eaccec654ff4f05e4cecac26399b6cb761361ca
Connor Malley <connor.malley@ucf.edu>
2024-08-21 22:04:21 -0500

    spiral

--- spiral_rect.lua
+++ spiral_rect.lua
@@ -42,16 +42,15 @@ local function spiral_rect(x, y, height, thickness)
             end
 
             for k=1,num_to_build do
-                utils.get_next_block()
-                utils.dig_place_up()
+                if k < num_to_build then
+                    utils.dig_place_up()
+                end
+                utils.dig_move_forward(1)
                 if (blocks_placed % thickness == 0) then
                     block_to_place = 1 - block_to_place
                 end
                 utils.getNextBlockByName(blocks[block_to_place + 1])
                 blocks_placed = blocks_placed + 1
-                if k < num_to_build then
-                    utils.dig_move_forward(1)
-                end
             end
             utils.rotate_clockwise(90)
             is_y_side = not is_y_side
