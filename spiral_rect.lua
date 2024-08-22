d2ddeb66f9a8662a67e6952e0c5203bd9d3f7be0
Connor Malley <connor.malley@ucf.edu>
2024-08-21 21:49:02 -0500

    spiral

--- spiral_rect.lua
+++ spiral_rect.lua
@@ -1,11 +1,37 @@
 local utils = require("utils")
 
+function getFirstTwoUniqueBlocks()
+    local foundBlocks = {}
+    local uniqueBlocks = {}
+
+    for i = 1, 16 do
+        local itemDetail = turtle.getItemDetail(i)
+        
+        if itemDetail then
+            local blockName = itemDetail.name
+
+            if not foundBlocks[blockName] then
+                table.insert(uniqueBlocks, blockName)
+                foundBlocks[blockName] = true
+            end
+
+            if #uniqueBlocks == 2 then
+                break
+            end
+        end
+    end
+
+    return uniqueBlocks
+end
+
 local function spiral_rect(x, y, height, thickness)
     utils.dig_move_up(-2)
     local is_y_side = false
     local num_to_build = 0
-    local block_to_place = 1
+    local block_to_place = 0
     local blocks_placed = 1
+
+    local blocks = getFirstTwoUniqueBlocks()
     for i=1,height do
         for j=1,4 do
             if is_y_side then 
@@ -18,7 +44,8 @@ local function spiral_rect(x, y, height, thickness)
                 utils.get_next_block()
                 utils.dig_place_up()
                 if (blocks_placed % thickness == 0) then
-                    block_to_place = block_to_place % 16 + 1
+                    block_to_place = 1 - block_to_place
+                    getNextBlockByName(blocks[block_to_place + 1])
                 end
                 turtle.select(block_to_place)
                 blocks_placed = blocks_placed + 1
